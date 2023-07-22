821baa9100211286db3dad4681989227ccb3a646
rabiaba <zainengr00@gmail.com>
2023-07-22 10:34:11 +0500

    space

--- src/modules/car/car_middlewware.js
+++ src/modules/car/car_middlewware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const authUser = (req, res, next) => {
+  
   const rf_token = req.headers.authorization.split(' ')[1];
   jwt.verify(rf_token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.status(400).json({ msg: 'you are not authorized.' });
